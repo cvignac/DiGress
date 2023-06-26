5df5382c2c50771e7766818c5e0a8d234e4c21ec
Clement Vignac <clement.vignac@epfl.ch>
2023-06-26 15:52:39 +0200

    Fixed metric logging.

--- src/metrics/train_metrics.py
+++ src/metrics/train_metrics.py
@@ -98,12 +98,11 @@ class TrainLossDiscrete(nn.Module):
         loss_E = self.edge_loss(flat_pred_E, flat_true_E) if true_E.numel() > 0 else 0.0
         loss_y = self.y_loss(pred_y, true_y) if true_y.numel() > 0 else 0.0
 
-
         if log:
             to_log = {"train_loss/batch_CE": (loss_X + loss_E + loss_y).detach(),
-                      "train_loss/X_CE": self.edge_loss.compute() if true_X.numel() > 0 else -1,
-                      "train_loss/E_CE": loss_E if true_E.numel() > 0 else -1,
-                      "train_loss/y_CE": self.node_loss.compute() if true_y.numel() > 0 else -1}
+                      "train_loss/X_CE": self.node_loss.compute() if true_X.numel() > 0 else -1,
+                      "train_loss/E_CE": self.edge_loss.compute() if true_E.numel() > 0 else -1,
+                      "train_loss/y_CE": self.y_loss.compute() if true_y.numel() > 0 else -1}
             wandb.log(to_log, commit=True)
         return loss_X + self.lambda_train[0] * loss_E + self.lambda_train[1] * loss_y
 
